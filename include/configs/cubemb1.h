987d967039a4ae35a36d8ac752cb78dc95419b93
Tim Small <tim@seoss.co.uk>
2020-12-09 13:38:20 +0000

    Rough draft u-boot support for Cube MB v1 against "sam4linux" fork.
    This is the minimum necessary u-boot diffset for use against the
    "sam4linux" u-boot fork.
    
    This should be used as a starting point to add support to mainline
    u-boot, and incorporate feedback from the mainline u-boot developers.

---
+++ include/configs/cubemb1.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration file for the South Coast Science Cube MB 1 Board.
+ *
+ * Copyright (C) 2020 South East Open Source Solutions Ltd.
+ *		      Tim Small <tim@seoss.co.uk>
+ */
+
+#include "sama5d2_xplained.h"

f9a84f38947b60d9c60b77a4df3d9cf9fa3b380e
Tim Small <tim@seoss.co.uk>
2024-10-11 09:08:48 +0100

    Override included CONFIG_BOOTCOMMAND for cube.

--- include/configs/cubemb1.h
+++ include/configs/cubemb1.h
@@ -6,4 +6,13 @@
  *		      Tim Small <tim@seoss.co.uk>
  */
 
+
 #include "sama5d2_xplained.h"
+
+/* Use sama5d2 xplained board configuration, except... */
+
+/* bootstrap + u-boot + env from eMMC (or SD Card) as appripriate */
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND	"fatload mmc " CONFIG_ENV_FAT_DEVICE_AND_PART " 0x21000000 at91-sama5d27_cubemb1.dtb; " \
+				"fatload mmc " CONFIG_ENV_FAT_DEVICE_AND_PART " 0x22000000 zImage; " \
+				"bootz 0x22000000 - 0x21000000"
